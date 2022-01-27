57d60e72c6deeaebf5be780f666482268a18c405
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-01-27 16:42:26 -0500

    add anither test

--- source/src/pages/SignUpPage.spec.js
+++ source/src/pages/SignUpPage.spec.js
@@ -1,8 +1,16 @@
 import SignUpPage from './SignUpPage.vue';
-import {render, screen } from '@testing-library/vue';
+import { render, screen } from '@testing-library/vue';
 import "@testing-library/jest-dom";
 
-it('has Sign Up Header', () => {
-    render(SignUpPage);
-    expect(screen.queryByRole('heading', {name: 'Sign Up'})).toBeInTheDocument();
-})
+describe("Sign Up Page", () => {
+    describe("Layout", () => {
+        it('has Sign Up Header', () => {
+            render(SignUpPage);
+            expect(screen.queryByRole('heading', { name: 'Sign Up' })).toBeInTheDocument();
+        })
+        it('has username input', () => {
+            const { container } = render(SignUpPage);
+            expect(container.querySelector("input")).toBeInTheDocument();
+        })
+    });
+});
--- source/src/pages/SignUpPage.vue
+++ source/src/pages/SignUpPage.vue
@@ -1,3 +1,6 @@
 <template>
+  <div>
     <h1>Sign Up</h1>
+    <input />
+  </div>
 </template>
