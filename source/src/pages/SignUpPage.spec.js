b52a86614e09821deaa0eb13023df51d835151dd
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-01-27 21:43:08 -0500

    add axios mocking and fetching

--- source/src/pages/SignUpPage.spec.js
+++ source/src/pages/SignUpPage.spec.js
@@ -2,6 +2,7 @@ import SignUpPage from './SignUpPage.vue';
 import { render, screen } from '@testing-library/vue';
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
+import axios from 'axios';
 
 describe("Sign Up Page", () => {
     describe("Layout", () => {
@@ -33,23 +34,47 @@ describe("Sign Up Page", () => {
         })
         it('disables the button initially', () => {
             render(SignUpPage);
-            const input = screen.queryByRole("button", {name: "Sign Up"});
+            const input = screen.queryByRole("button", { name: "Sign Up" });
             expect(input).toBeInTheDocument();
             expect(input).toBeDisabled();
         })
     });
 
     describe('Interactions', () => {
-        // it('enables the button when the password and password repeat fields have the same values', async () => {
-        //     render(SignUpPage);
-        //     const passwordInput = screen.queryAllByAltText("Password")
-        //     const repeatPasswordInput = screen.queryAllByAltText("Repeat Password")
-        //     await userEvent.type(passwordInput, "P4ssword");
-        //     await userEvent.type(repeatPasswordInput, "P4ssword");
+        it('enables the button when the password and password repeat fields have the same values', async () => {
+            render(SignUpPage);
+            const passwordInput = screen.queryByLabelText("Password")
+            const repeatPasswordInput = screen.queryByLabelText("Repeat Password")
+            await userEvent.type(passwordInput, "P4ssword");
+            await userEvent.type(repeatPasswordInput, "P4ssword");
 
-        //     const input = screen.queryByRole("button", { name: "Sign Up" });
-        //     expect(input).toBeInTheDocument();
-        //     expect(input).toBeEnabled();
-        // })
+            const input = screen.queryByRole("button", { name: "Sign Up" });
+            expect(input).toBeInTheDocument();
+            expect(input).toBeEnabled();
+        })
+
+        it('sends username, email and password to backend after clicking the button', async () => {
+            render(SignUpPage);
+            const usernameInput = screen.queryByLabelText("Username")
+            const emailInput = screen.queryByLabelText("E-mail")
+            const passwordInput = screen.queryByLabelText("Password")
+            const repeatPasswordInput = screen.queryByLabelText("Repeat Password")
+            await userEvent.type(usernameInput, "user1");
+            await userEvent.type(emailInput, "user@email");
+            await userEvent.type(passwordInput, "P4ssword");
+            await userEvent.type(repeatPasswordInput, "P4ssword");
+
+            const button = screen.queryByRole("button", { name: "Sign Up" });
+
+            
+            let mock = jest.fn();
+            axios.post = mock;
+            
+            await userEvent.click(button);
+
+            const firstCall = mock.mock.calls[0];
+            const body = firstCall[1];
+            expect(body).toEqual({ username: 'user1', email: 'user@email', password: 'P4ssword' });
+        })
     });
 });
--- source/src/pages/SignUpPage.vue
+++ source/src/pages/SignUpPage.vue
@@ -3,10 +3,10 @@
     <h1>Sign Up</h1>
 
     <label for="username">Username</label>
-    <input placeholder="username" id="username" :value="username" />
+    <input placeholder="username" id="username" v-model="username" />
 
     <label for="email">E-mail</label>
-    <input placeholder="email" id="email" :value="email" />
+    <input placeholder="email" id="email" v-model="email" />
 
     <label for="password">Password</label>
     <input
@@ -24,11 +24,13 @@
       v-model="repeatPassword"
     />
 
-    <button :disabled="isDisabledComputed">Sign Up</button>
+    <button :disabled="isDisabledComputed" @click="submit">Sign Up</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: "SignUpPage",
   data() {
@@ -40,6 +42,13 @@ export default {
     };
   },
   methods: {
+      submit() {
+          axios.post('/api/1.0/users', {
+              username: this.username,
+              email: this.email,
+              password: this.password
+          })
+      },
     onChangePassword(event) {
       this.password = event.target.value;
     },
